96663760f4fdb8c91ca7ae89d6178babda661c02
KKUMI <gayoung801@naver.com>
2023-03-27 21:06:09 +0900

    ArrayDemo

---
+++ src/main/java/practice/ArrayDemo.java
@@ -0,0 +1,11 @@
+package practice;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public long solution() {
+        long answer = 0;
+        long n = 118372;
+        return answer;
+    }
+}

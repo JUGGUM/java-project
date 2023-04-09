42c44ac546afdd4976ff9aa36220b8741d5c792c
KKUMI <gayoung801@naver.com>
2023-04-09 16:12:40 +0900

    TriangleDemo

--- src/main/java/practice/ArrayDemo.java
+++ src/main/java/practice/ArrayDemo.java
@@ -1,11 +1,15 @@
 package practice;
 
+import java.sql.SQLSyntaxErrorException;
 import java.util.Arrays;
 
 public class ArrayDemo {
     public long solution() {
         long answer = 0;
         long n = 118372;
+        if(n == 2){
+            throw new RuntimeException();
+        }
         return answer;
     }
 }
---
+++ src/main/java/practice/TriangleDemo.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class TriangleDemo {
+    public int solution() {
+        int[] sides = {1,2,3};
+        Arrays.sort(sides);
+
+        if( sides[2] <= sides[0] + sides[1] ) {
+            return 1;
+        }else {
+            return 2;
+        }
+
+    }
+}

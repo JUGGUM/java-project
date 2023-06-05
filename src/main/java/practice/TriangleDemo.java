848a00ff11b434a089a1d72c169d510b0ba99dfd
KKUMI <gayoung801@naver.com>
2023-06-05 15:34:10 +0900

    Update TriangleDemo.java

--- src/main/java/practice/TriangleDemo.java
+++ src/main/java/practice/TriangleDemo.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 
 class TriangleDemo {
     public int solution() {
-        int[] sides = {1,2,3};
+        int[] sides = {1, 2, 3};
         Arrays.sort(sides);
 
-        if( sides[2] <= sides[0] + sides[1] ) {
+        if (sides[2] <= sides[0] + sides[1]) {
             return 1;
-        }else {
+        } else {
             return 2;
         }
 

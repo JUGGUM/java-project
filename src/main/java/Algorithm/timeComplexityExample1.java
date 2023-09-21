b0c2a7b030dbd577e5e62996559259ff119a70e7
JUGGUM <gayoung801@naver.com>
2023-09-21 11:49:57 +0900

    Feat : 시간복잡도 N timeComplexityExample1

---
+++ src/main/java/Algorithm/timeComplexityExample1.java
@@ -0,0 +1,15 @@
+package Algorithm;
+
+public class timeComplexityExample1 {
+    public static void main(String[] args) {
+        // 1~100사이 값 랜덤 선택
+        // 빅-오메가 1번 빅-세타 N/2 빅-오 N
+        int findNumber = (int)(Math.random() * 100);
+        for(int i = 0; i<100; i++){
+            if(i == findNumber){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}

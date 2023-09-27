7af742d6db9e3b7934ca74136164ef8395aedc22
JUGGUM <gayoung801@naver.com>
2023-09-27 11:18:57 +0900

    Update : timeComplexityExample1

--- src/main/java/Algorithm/TimeComplexityExample1.java
+++ src/main/java/Algorithm/TimeComplexityExample1.java
@@ -1,6 +1,6 @@
 package Algorithm;
 
-public class timeComplexityExample1 {
+public class TimeComplexityExample1 {
     public static void main(String[] args) {
         // 1~100사이 값 랜덤 선택
         // 빅-오메가 1번 빅-세타 N/2 빅-오 N

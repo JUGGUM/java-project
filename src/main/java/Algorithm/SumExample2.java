0ca653e0d9977879800ce50fae6e1934c4aac0a5
JUGGUM <gayoung801@naver.com>
2023-10-16 15:27:34 +0900

    Feat : SumExample

---
+++ src/main/java/Algorithm/SumExample2.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/***
+ * 구간합 사용빈도수 높다함
+ */
+public class SumExample2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    }
+}
